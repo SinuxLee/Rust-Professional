8af90397a2e9e85b7c40959f9721b6a2779a2063
Your Name <you@example.com>
2025-02-19 14:06:43 +0800

    support rust-analyzer

---
+++ exercises/easy/mod.rs
@@ -0,0 +1,40 @@
+#[cfg(test)]
+mod algorithm1;
+#[cfg(test)]
+mod algorithm2;
+#[cfg(test)]
+mod algorithm3;
+#[cfg(test)]
+mod algorithm4;
+#[cfg(test)]
+mod algorithm5;
+#[cfg(test)]
+mod algorithm6;
+#[cfg(test)]
+mod algorithm7;
+#[cfg(test)]
+mod algorithm8;
+#[cfg(test)]
+mod algorithm9;
+#[cfg(test)]
+mod algorithm10;
+#[cfg(test)]
+mod algorithm11;
+#[cfg(test)]
+mod algorithm12;
+#[cfg(test)]
+mod algorithm13;
+#[cfg(test)]
+mod algorithm14;
+#[cfg(test)]
+mod algorithm15;
+#[cfg(test)]
+mod algorithm16;
+#[cfg(test)]
+mod algorithm17;
+#[cfg(test)]
+mod algorithm18;
+#[cfg(test)]
+mod algorithm19;
+#[cfg(test)]
+mod algorithm20;
